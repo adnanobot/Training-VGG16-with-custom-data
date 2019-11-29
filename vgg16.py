259c86714e2f865adc5ba1b345a57ef9072ff067
mahmed <mainuddin.ahmed@iav.de>
2019-11-29 17:12:36 +0100

    1. complete code for training 2. created requirement file

--- vgg16.py
+++ vgg16.py
@@ -1,20 +1,35 @@
+# if importing keras directly creates issues because of the tensorflow 2.0
+# so importing the following way is working
+# import tensorflow.keras as k
+
 import keras
 from keras.layers import Dense
 from keras import Sequential
 from keras.layers import Activation
 from keras.optimizers import Adam
 from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16
 
-# train_path = 
-# test_path = 
-# valid_path = 
+# loading data
+train_path = "./Data/train"
+test_path =  "./Data/test"
+valid_path =  "./Data/validation"
 
-vgg = keras.applications.vgg16.VGG16()
+train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size = (224, 224),
+                classes = ["Screwdriver", "Eraser"],  batch_size = 10)
+test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size = (224, 224),
+                classes = ["Screwdriver", "Eraser"],  batch_size = 10)
+valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size = (224, 224),
+                classes = ["Screwdriver", "Eraser"],  batch_size = 10)
+
+
+vgg = VGG16()
 # vgg.summary()
 
 type(vgg) # this inported model is a keras.engine.training.Model type not Sequential type
 
-# so Model >> Sequential
+# so convert Model >> Sequential
 classifier = Sequential()
 for layer in vgg.layers:
     classifier.add(layer)
@@ -29,8 +44,8 @@ for layer in classifier.layers:
 # adding new final layer for classifying only screw-driver and pen
 classifier.add(Dense(2, activation = 'softmax')) 
 
-classifier.compile(optimizer = optimizers.Adam(learning_rate = 0.0001), loss='categorical_crossentropy',
+classifier.compile(optimizer = Adam(learning_rate = 0.0001), loss='categorical_crossentropy',
                                 metrics=['accuracy'])
-classifier.fit_generator(batch_size = 20, epochs = 5, steps_per_epoch = 4,
-                        validatation_data = valid_batches, validation_steps = 4, verbose = 2)
+classifier.fit_generator(train_batches, epochs = 20, steps_per_epoch = 4,
+                        validation_data = valid_batches, validation_steps = 4, verbose = 2)
 classifier.summary()
