8de5c6d4d75d285389b7f14bfd5fcf1f8e08dd86
mahmed <mainuddin.ahmed@iav.de>
2019-12-02 10:28:38 +0100

    updates

--- vgg16.py
+++ vgg16.py
@@ -1,3 +1,7 @@
+"""
+    Reference for source code: https://www.youtube.com/watch?v=INaX55V1zpY&list=PLZbbT5o_s2xrwRnXk_yCPtnqqo4_u2YGL&index=14
+
+"""
 # if importing keras directly creates issues because of the tensorflow 2.0
 # so importing the following way is working
 # import tensorflow.keras as k
@@ -14,7 +18,7 @@ from keras.applications.vgg16 import VGG16
 # loading data
 train_path = "./Data/train"
 test_path =  "./Data/test"
-valid_path =  "./Data/validation"
+valid_path =  "./Data/valid"
 
 train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size = (224, 224),
                 classes = ["Screwdriver", "Eraser"],  batch_size = 10)
@@ -23,7 +27,7 @@ test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size =
 valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size = (224, 224),
                 classes = ["Screwdriver", "Eraser"],  batch_size = 10)
 
-
+"""
 vgg = VGG16()
 # vgg.summary()
 
@@ -49,3 +53,4 @@ classifier.compile(optimizer = Adam(learning_rate = 0.0001), loss='categorical_c
 classifier.fit_generator(train_batches, epochs = 20, steps_per_epoch = 4,
                         validation_data = valid_batches, validation_steps = 4, verbose = 2)
 classifier.summary()
+"""
